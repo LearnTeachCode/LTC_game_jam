f181999693d0f52d73bdb5e52fc6a62840149275
Romand Tse <romand.tse@gmail.com>
2018-11-15 21:12:25 -0800

    added more values

--- client/config/config.js
+++ client/config/config.js
@@ -2,6 +2,24 @@ var config = {
  "loader":{
    "x": 80,
    "y": 150,
-   "text": "loading..."
+   "text": "loading...",
+   "font": "30px Courier",
+   "fill": "#fff"
  },
+
+ "screen":{
+   "x":375,
+   "y":812
+ },
+
+ "char":{
+   "dimX":60,
+   "dimY":70,
+   "color":{},
+   "speedX":1
+ },
+
+ "stage":{
+   "speedY":1
+ }
 }
