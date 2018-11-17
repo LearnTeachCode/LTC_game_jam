498f90504bf560c4512a124f8f7ce5a60309d1a1
Hong Luu <hluu114@gmail.com>
2018-11-17 08:08:28 -0800

    Ticket-19 Cleaned up duty

--- client/js/boot.js
+++ client/js/boot.js
@@ -7,62 +7,69 @@ function displayBootText(gameText, newString, timer){
     }, timer);
 }
 
-// Retrieve user data
-function getUserData(){
-    // var text4 = game.add.text(0, 300, '', {font: '30px Courier', fill: '#fff'});
-    // var text5 = game.add.text(0, 350, '', {font: '30px Courier', fill: '#fff'});
+// get user device
+function getUserDevice(){
     var newDataText = '';
-
-    // get user device
     newDataText = (game.device.android) ? newDataText + 'android,': newDataText;
     newDataText = (game.device.iphone) ? newDataText + 'iphone,': newDataText;
     newDataText = (game.device.ipad) ? newDataText + 'ipad,': newDataText;
     newDataText = (game.device.windows) ? newDataText + 'windows,': newDataText;
     newDataText = (game.device.iOS) ? newDataText + 'iOS,': newDataText;
     newDataText = (game.device.linux) ? newDataText + 'linux,': newDataText;
+
     if(newDataText != ''){
         // remove last comma in newDataText
         newDataText = newDataText.substring(0, newDataText.length - 1);
-        userData['device'] = newDataText;
     }
+    return newDataText;
+}
 
-    // get user browser
-    newDataText = '';
+// get user browser
+function getUserBrowser(){
+    var newDataText = '';
     newDataText = (game.device.chrome) ? newDataText + 'chrome,': newDataText;
     newDataText = (game.device.safari) ? newDataText + 'safari,': newDataText;
     newDataText = (game.device.firefox) ? newDataText + 'firefox,': newDataText;
     if(newDataText != ''){
         // remove last comma in newDataText
         newDataText = newDataText.substring(0, newDataText.length - 1);
-        userData['browser'] = newDataText;
     }
+    return newDataText;
+}
 
-    // check supported audio from user device
-    newDataText = '';
+// check supported audio from user device
+function getUserAudioType(){
+    var newDataText = '';
     newDataText = (game.device.mp3) ? newDataText + 'mp3,': newDataText;
     newDataText = (game.device.wav) ? newDataText + 'wav,': newDataText;
     newDataText = (game.device.ogg) ? newDataText + 'ogg,': newDataText;
     if(newDataText != ''){
         // remove last comma in newDataText
         newDataText = newDataText.substring(0, newDataText.length - 1);
-        userData['audiotype'] = newDataText;
     }
+    return newDataText;
 }
+
+// Retrieve user data
+function getUserData(){
+    userData['device'] = getUserDevice();
+    userData['browser'] = getUserBrowser();
+    userData['audiotype'] = getUserAudioType();
+}
+
 var bootState = {
-
     create: function () {
-        var timerDelta = 500;
-
+        var timerDelta = 500;   // how much wait time to increment
         var bootString = 'Booting up';
-        var text1 = game.add.text(0, 150, 'Thanks for playing', {font: '30px Courier', fill: '#fff'});
-        var text2 = game.add.text(0, 200, gameTitle, {font: '30px Courier', fill: '#fff'});
-        var text3 = game.add.text(0, 250, bootString, {font: '30px Courier', fill: '#fff'});
+        var gametextThankYou = game.add.text(0, 150, 'Thanks for playing', {font: '30px Courier', fill: '#fff'});
+        var gametextGameTitle = game.add.text(0, 200, gameTitle, {font: '30px Courier', fill: '#fff'});
+        var gametextBootString = game.add.text(0, 250, bootString, {font: '30px Courier', fill: '#fff'});
 
         timer = 500;
         for(i = 1; i < 5; i++){
             timer += timerDelta;
             bootString += '.';
-            displayBootText(text3, bootString, timer);
+            displayBootText(gametextBootString, bootString, timer);
         }
 
         setTimeout(function(){
@@ -72,19 +79,20 @@ var bootState = {
         // debug test
         if(DEBUG){
             setTimeout(function(){
-                var text4 = game.add.text(0, 300, 'hello', {font: '30px Courier', fill: '#fff'});
+                var gametextDebugger = game.add.text(0, 300, 'hello', {font: '30px Courier', fill: '#fff'});
                 bootString ='';
                 for (var i in userData){
                     bootString += userData[i] +',';
                 }
-                text4.setText(bootString);
+                gametextDebugger.setText(bootString);
             }, timer += timerDelta);
         }
 
         // move on to next state
         setTimeout(function () {
             
-            // opening config file
+            // TODO: determine if writing to player's computer is something to move forward with
+            // ignore for now
             //var file = fopen('../config/ltc_config.txt')
 
             //Initial GameSystem (Arcade, P2, Ninja)
