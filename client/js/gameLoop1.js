a4ec28c1b3dd67f2e05731ee9827047a4e8ea1b2
Loaf <clmus85@hotmail.com>
2018-11-17 17:42:38 -0800

    Controls Attempt
    Trying to work out a problem with controls, not sure how to fit the bottom half of gameLoop1.js into the picture.

--- client/js/gameLoop1.js
+++ client/js/gameLoop1.js
@@ -1,7 +1,23 @@
 var gameLoop1State = {
 
-    update: function () {
-        console.log("loop");
+    create: function () {
+        var cursors;
+        cursors = game.input.keyboard.createCursorKeys()
+        player = game.add.image(310, 400, 'placeholder');
+        player.anchor.setTo(0.5, 0.5);
+        game.physics.enable(player, Phaser.Physics.ARCADE);
     }
-
-}
+};
+//function update() {
+//          Reset the player, then check for movement keys
+//        player.body.velocity.setTo(0, 0);
+//
+//        if (cursors.left.isDown)
+//        {
+//            player.body.velocity.x = -200;
+//        }
+//        else if (cursors.right.isDown)
+//        {
+//            player.body.velocity.x = 200;
+//        }
+//        }
