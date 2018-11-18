31689abe87be3eeddf81fdeeb0a38bebe0b41d48
Loaf <clmus85@hotmail.com>
2018-11-17 20:16:42 -0800

    It moves! :D
    Restructured gameLoop1State, added preload and update functions, as well as basic left and right keyboard control. Some redundancy was added for testing

--- client/js/gameLoop1.js
+++ client/js/gameLoop1.js
@@ -1,23 +1,24 @@
 var gameLoop1State = {
-
+    preload: function() {
+        game.load.image('placeholder', 'assets/img/placeholder.png');
+    },
     create: function () {
-        var cursors;
-        cursors = game.input.keyboard.createCursorKeys()
         player = game.add.image(310, 400, 'placeholder');
+        player2 = game.add.image(100, 100, 'boiler-logo')
         player.anchor.setTo(0.5, 0.5);
         game.physics.enable(player, Phaser.Physics.ARCADE);
-    }
+    },
+
+    update: function() {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+        {
+            player2.x -= 8;
+            player.x -= 8;
+        }
+        else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+        {
+            player2.x += 8;
+            player.x += 8;
+        }
+        }
 };
-//function update() {
-//          Reset the player, then check for movement keys
-//        player.body.velocity.setTo(0, 0);
-//
-//        if (cursors.left.isDown)
-//        {
-//            player.body.velocity.x = -200;
-//        }
-//        else if (cursors.right.isDown)
-//        {
-//            player.body.velocity.x = 200;
-//        }
-//        }
