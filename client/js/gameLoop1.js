a8841611b13df4779f1a7decb2593a716dc10692
Loaf <clmus85@hotmail.com>
2018-11-17 20:58:38 -0800

    More movement!
    changed image to sprite as I believe the physics are applied to sprites and not static images, added simple mouse controls.

--- client/js/gameLoop1.js
+++ client/js/gameLoop1.js
@@ -3,8 +3,8 @@ var gameLoop1State = {
         game.load.image('placeholder', 'assets/img/placeholder.png');
     },
     create: function () {
-        player = game.add.image(310, 400, 'placeholder');
-        player2 = game.add.image(100, 100, 'boiler-logo')
+        player = game.add.sprite(310, 400, 'placeholder');
+        player2 = game.add.sprite(100, 100, 'boiler-logo')
         player.anchor.setTo(0.5, 0.5);
         game.physics.enable(player, Phaser.Physics.ARCADE);
     },
@@ -19,6 +19,18 @@ var gameLoop1State = {
         {
             player2.x += 8;
             player.x += 8;
+        }
+        else if (game.input.mousePointer.isDown)
+        {
+            game.physics.arcade.moveToPointer(player, 400);
+            if (Phaser.Rectangle.contains(player.body, game.input.x, game.input.y))
+            {
+            player.body.velocity.setTo(0, 0);
+            }
+        }
+        else
+        {
+            player.body.velocity.setTo(0, 0);
         }
         }
 };
