381fad01fadde9a1cf9cd945c4fe3e26b4a1b24e
Eric <ericqu96@gmail.com>
2018-11-18 18:23:50 -0800

    implemented a more direct way of moving the player sprite with mouse, removed functionality for arrow keys (for now)
    also commented out debugger stuff, it was getting in the way

--- client/js/menu.js
+++ client/js/menu.js
@@ -2,8 +2,8 @@ var menuState = {
 
     create: function () {
 
-        game.add.plugin(Phaser.Plugin.Debug);
-        game.add.plugin(Phaser.Plugin.Inspector);
+        //game.add.plugin(Phaser.Plugin.Debug);
+        //game.add.plugin(Phaser.Plugin.Inspector);
         game.add.plugin(PhaserSuperStorage.StoragePlugin);
         game.add.plugin(PhaserInput.Plugin);
         
