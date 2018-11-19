440489e7ed0d82a2748b26fb4fcf6411444352aa
e-a-n <ean.director@gmail.com>
2018-11-18 19:41:08 -0800

    refactoring code.  getting rid of globals, making sure functions have entry/exit points instead

--- client/js/boot.js
+++ client/js/boot.js
@@ -1,10 +1,62 @@
-var gameTitle = 'LTC_GameJam_Colors-thingy-stuff';
+const bootState = {};
+bootState.gameTitle = 'LTC_GameJam_Colors-thingy-stuff';
+// Retrieve user data
+/*
+   From Ean: Hong, make sure our functions have an entry/exit point so we can make them testible
+*/
+bootState.setUserData = () => {
+    let data = {};
+    data['device'] = getUserDevice();
+    data['browser'] = getUserBrowser();
+    data['audiotype'] = getUserAudioType();
 
+    return data;
+}
 // displayBootText sets up a delay by given timer before displaying provided string
-function displayBootText(gameText, newString, timer){
+bootState.displayBootText = (gameText, newString, timer) => {
     setTimeout(function () {
         gameText.setText(newString);
     }, timer);
+}
+
+bootState.userData = bootState.setUserData();
+bootState.debugBootState = () => {
+    if(DEBUG){
+        setTimeout(function(){
+            var gametextDebugger = game.add.text(0, 300, 'hello', {font: '30px Courier', fill: '#fff'});
+            bootString ='';
+            let userData = bootState.userData;
+            for (var i in userData){
+                bootString += userData[i] +' ';
+            }
+            gametextDebugger.setText(bootString);
+        }, timer);
+    }
+}
+bootState.create = () => {
+    var timerDelta = 500;   // how much wait time to increment
+    var bootString = 'Booting up';
+    var gametextThankYou = game.add.text(0, 150, 'Thanks for playing', {font: '30px Courier', fill: '#fff'});
+    var gametextGameTitle = game.add.text(0, 200, bootState.gameTitle, {font: '30px Courier', fill: '#fff'});
+    var gametextBootString = game.add.text(0, 250, bootString, {font: '30px Courier', fill: '#fff'});
+
+    // animate "booting..." string
+    var timer = 500;
+    for(i = 1; i < 5; i++){
+        timer += timerDelta;
+        bootString += '.';
+        bootState.displayBootText(gametextBootString, bootString, timer);
+    }
+
+    // retrieve device info and store into userData map (might be located in stateManager.js)
+    setTimeout(function(){
+        getUserData();
+    }, timer += timerDelta);
+
+    if(DEBUG)
+        bootState.debugBootState(timer += timerDelta);
+
+    startState('load', timer += timerDelta);
 }
 
 // get user device
@@ -50,12 +102,7 @@ function getUserAudioType(){
     return newDataText;
 }
 
-// Retrieve user data
-function getUserData(){
-    userData['device'] = getUserDevice();
-    userData['browser'] = getUserBrowser();
-    userData['audiotype'] = getUserAudioType();
-}
+
 
 // move on to next state
 function startState(stateName, timer){
@@ -87,30 +134,4 @@ function debugBootState(timer){
     }
 }
 
-var bootState = {
-    create: function () {
-        var timerDelta = 500;   // how much wait time to increment
-        var bootString = 'Booting up';
-        var gametextThankYou = game.add.text(0, 150, 'Thanks for playing', {font: '30px Courier', fill: '#fff'});
-        var gametextGameTitle = game.add.text(0, 200, gameTitle, {font: '30px Courier', fill: '#fff'});
-        var gametextBootString = game.add.text(0, 250, bootString, {font: '30px Courier', fill: '#fff'});
 
-        // animate "booting..." string
-        var timer = 500;
-        for(i = 1; i < 5; i++){
-            timer += timerDelta;
-            bootString += '.';
-            displayBootText(gametextBootString, bootString, timer);
-        }
-
-        // retrieve device info and store into userData map (might be located in stateManager.js)
-        setTimeout(function(){
-            getUserData();
-        }, timer += timerDelta);
-
-        if(DEBUG)
-            debugBootState(timer += timerDelta);
-
-        startState('load', timer += timerDelta);
-    }
-};
