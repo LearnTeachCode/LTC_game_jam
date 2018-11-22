e0a4004cc1539e1f3f02595c23af929845416daf
Hong Luu <hluu114@gmail.com>
2018-11-21 18:07:03 -0800

    Ticket-59 Further explanation

--- client/js/tests/testConfig.js
+++ client/js/tests/testConfig.js
@@ -6,10 +6,16 @@
 const testConfig = {};
 
 testConfig.testConfigInit = () => {
-    // test config init values
+    /**
+     * Syntax for assert equal is generally: assert.equal(value1, value2, "Some Error Message");
+     * Other test libraries may be different, unitjs library makes unitjs a global variable, thus the syntax is:
+     * unitjs.assert.equal(value1, value2, "Some Error Message");
+     * Typically, we determine what the expected outcome is, then compare it with the actual value from running the process.
+     * Having end point from functions makes creating these tests much easier.
+     * If test fails, then it will raise an error and stop the rest of the program.
+     */
 
-    // syntax for assert equal: assert.equal(value1, value2, "Some Error Message");
-    // if test fails, then it will raise an error and stop the rest of the program
+     // test config init values
     let expected = 640;     // current default value: 640
     let actual = config.init.screenWidth;
     unitjs.assert.equal(actual, expected, "TestConfigInit failed! Config.init.screenWidth not equal to " + expected);
