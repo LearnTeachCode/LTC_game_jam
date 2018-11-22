5ea0eeb9eb2899775b16dddba38bda0e0ae24c8a
Hong Luu <hluu114@gmail.com>
2018-11-21 17:55:35 -0800

    Ticket-59 Added explanation to first test

--- client/js/tests/testConfig.js
+++ client/js/tests/testConfig.js
@@ -7,8 +7,14 @@ const testConfig = {};
 
 testConfig.testConfigInit = () => {
     // test config init values
+
+    // syntax for assert equal: assert.equal(value1, value2, "Some Error Message");
+    // if test fails, then it will raise an error and stop the rest of the program
     let expected = 640;     // current default value: 640
-    unitjs.assert.equal(config.init.screenWidth, expected, "TestConfigInit failed! Config.init.screenWidth not equal to " + expected);
+    let actual = config.init.screenWidth;
+    unitjs.assert.equal(actual, expected, "TestConfigInit failed! Config.init.screenWidth not equal to " + expected);
+
+    // code can be reduced 
     expected = 480;     // current default value: 480
     unitjs.assert.equal(config.init.screenHeight, expected, "TestConfigInit failed! Config.init.screenHeight not equal to " + expected);
 }
