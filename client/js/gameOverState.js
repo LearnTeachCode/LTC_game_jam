76a2e3d3119a78d0c0f957f0c79bd7414f9c7fa7
Eric <ericqu96@gmail.com>
2018-11-22 23:05:11 -0800

    added template game over screen that displays score and restart button
    pressing space during gameloop triggers a game over while in debug state

--- client/js/gameOverState.js
+++ client/js/gameOverState.js
@@ -1,10 +1,23 @@
-
 var gameOverState = {
+    restartGame: function () {
+        game.state.start('gameLoop');
+    },
+
+    // TODO: Move this into load.js
+    preload: function () {
+        game.load.image('restartButton', 'assets/img/startButton.png');
+    },
 
     create: function () {
+        let gameOverLabel = game.add.text(w / 2, 0.25 * h, 'Game Over!', { font: '35px Courier', fill: '#fff' });
+        gameOverLabel.anchor.setTo(0.5);
+        let finalScoreLabel = game.add.text(w / 2, 0.33 * h, 'Final Score:', { font: '30px Courier', fill: '#fff' });
+        finalScoreLabel.anchor.setTo(0.5);
+        let finalScore = game.add.text(w / 2, 0.45 * h, playerScore, { font: '30px Courier', fill: '#fff' });
+        finalScore.anchor.setTo(0.5);
 
-        // Game over logic goes here
-
+        let restartButton = game.add.button(w / 2, 2 / 3 * h, 'restartButton', gameOverState.restartGame, this, 0, 0, 0);
+        restartButton.anchor.setTo(0.5);
     }
 
 };
--- client/js/menu.js
+++ client/js/menu.js
@@ -1,12 +1,6 @@
 var menuState = {
-    sprites: [],
-
     startGame: function () {
-        // TODO: start game loop
-        menuState.sprites.forEach(function (sprite) {
-            sprite.destroy();
-        });
-        game.add.text(0, 0, "you are now playing the game,\nand you just lost the game", { font:'50px', fill: '#fff' });
+        game.state.start('gameLoop');
     },
 
     // TODO: Move this into load.js
@@ -16,21 +10,12 @@ var menuState = {
     },
 
     create: function () {
-
-        //game.add.plugin(Phaser.Plugin.Debug);
-        //game.add.plugin(Phaser.Plugin.Inspector);
-        game.add.plugin(PhaserSuperStorage.StoragePlugin);
-        game.add.plugin(PhaserInput.Plugin);
-
         let mainMenuBackground = game.add.image(w / 2, h / 2, 'mainMenuBackground');
-        menuState.sprites.push(mainMenuBackground);
         mainMenuBackground.anchor.setTo(0.5);
         mainMenuBackground.scale.x = mainMenuBackground.scale.y = getScaleValueToEnvelopeRect(mainMenuBackground.width, mainMenuBackground.height, w, h);
 
-        let startButton = game.add.button(w / 2, 2 / 3 * h, 'startButton', menuState.startGame, this, 100, 100, 100);
-        menuState.sprites.push(startButton);
+        let startButton = game.add.button(w / 2, 2 / 3 * h, 'startButton', menuState.startGame, this, 0, 0, 0);
         startButton.anchor.setTo(0.5)
-        //startButton.useHandCursor = true;
     }
 };
 
